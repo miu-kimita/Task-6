d4726d51d1262832daa2144e12a72f05ace8ba29
miu-kimita <miu.6ki26.bigdaddy@gmail.com>
2021-05-03 05:56:53 +0000

    [Finish]task-3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,6 +7,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @user = @book.user
     @new_book = Book.new
+    @book_comment = BookComment.new
   end
 
   def index
