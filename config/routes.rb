1b7633645a9f0b33f6fa92dbbae4f21313c91571
miu-kimita <miu.6ki26.bigdaddy@gmail.com>
2021-05-05 04:26:49 +0000

    [Finish]Task-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
+  get 'search' => 'searches#search'
   get 'home/about' => 'homes#about'
   resources :users,only: [:show,:index,:edit,:update] do
     resource :relationships, only: [:create, :destroy]
